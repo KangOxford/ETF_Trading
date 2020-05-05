e2807716521ce5c0cf4b239652980e0df345d98c
Terry Stanley <37290277+FinTechNJU@users.noreply.github.com>
2020-05-05 16:28:53 +0800

    自动读取上交所每日申赎清单和申赎券

--- __init__.py
+++ __init__.py
@@ -13,12 +13,13 @@ from ETF_Data import *
 if __name__=="__main__":
     info_list_ETF_PR_list = []
     info_list_ETF_constitution = []
-    for i in range(20,204):
+    for i in range(1, 300):
         etf_crawler = ETF_Crawler(i)
-        etf_data = ETF_Data(etf_crawler.dataRaw)
+        etf_data = ETF_Data(etf_crawler.dataRaw, etf_crawler.num_code)
         etf_data_df1, etf_data_df2 = etf_data.ETF_PR_list, etf_data.ETF_constitution
         info_list_ETF_PR_list.append(etf_data.ETF_PR_list)
         info_list_ETF_constitution.append(etf_data.ETF_constitution)
+    print(ETF_Data.valid_count)
+    print(ETF_Data.valid_list)
 
-# 第20个解码有问题，明早起来继续做
-# 'gb2312' codec can't decode byte 0xbb in position 61: illegal multibyte sequence
+# 为了减轻服务器任务，应该增加一个文件读写操作
