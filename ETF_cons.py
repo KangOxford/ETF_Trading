e03d7a203f4c2327a3e0ff540302aadbfadc4071
FinTech_NJU <fintech_nju@foxmail.com>
2020-05-27 09:30:58 +0800

    readme

--- ETF_cons.py
+++ ETF_cons.py
@@ -4,18 +4,25 @@ Created on Tue May  5 19:02:20 2020
 
 @author: hs101
 """
-
+#%%
 import pandas as pd
 
 df1 = pd.read_csv("./ETF_cons/" + "0_ETF_cons510050" + ".csv")
 df2 = pd.read_csv("./ETF_cons/" + "0_ETF_cons510180" + ".csv")
 # 获得基本数据
-
+#%% [markdown]
 #writer = pd.ExcelWriter('自定义.xlsx')
+# %%
+
+# %%
+
+# %%
+
+# %%
 #df1.to_excel(writer, sheet_name='自定义sheet_name0')
 #df2.to_excel(writer, sheet_name='自定义sheet_name1')
 #writer.save()
-
+#%%
 with pd.ExcelWriter("./ETF_cons/" + 'a.xls', mode = "w+", decoding='utf-8') as writer:
     df1.to_excel(writer, 'sheet1')
     df2.to_excel(writer, 'sheet2')
